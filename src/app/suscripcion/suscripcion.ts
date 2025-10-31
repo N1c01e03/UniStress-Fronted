c6d663a9ca286c6d4b593b48acfcb49493f28f82
Aricatri <aricatri@gmail.com>
2025-10-30 20:27:57 -0500

    add:MySubscription

--- src/app/suscripcion/suscripcion.ts
+++ src/app/suscripcion/suscripcion.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-suscripcion',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, DatePipe],
   templateUrl: './suscripcion.html',
-  styleUrl: './suscripcion.css',
+  styleUrls: ['./suscripcion.css'],
 })
 export class Suscripcion {
 
+  constructor(private router: Router) {}
+
+  plan = 'Premium';
+  estado: 'Activo' | 'Cancelado' = 'Activo';
+  inicio = new Date(2025, 8, 1);
+  fin    = new Date(2026, 9, 1);
+  beneficios = [
+    'Sesiones ilimitadas',
+    'Acceso a especialistas',
+    'Prioridades de atención'
+  ];
+
+  cancelar() {
+    this.estado = 'Cancelado';
+  }
+
+  renovar() {
+    this.router.navigate(['/pagos']); // ✅ ir a pantalla pagos
+  }
 }
